fc182a68ef180902d1a17e881dc61ffc6ba8e45a
farhanasysyifa830-cpu <farhanasysyifa830@gmail.com>
2026-07-11 09:28:01 +0700

    Add files via upload

---
+++ UAS_JasaJoki/db_joki (1).sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 11, 2026 at 04:21 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_joki`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+
+CREATE TABLE `chat` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `sender` enum('admin','customer') NOT NULL,
+  `pesan` text NOT NULL,
+  `dibaca` enum('0','1') DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `chat`
+--
+
+INSERT INTO `chat` (`id`, `user_id`, `sender`, `pesan`, `dibaca`, `created_at`) VALUES
+(1, 3, 'customer', 'min\r\n', '0', '2026-07-11 02:03:59'),
+(2, 3, 'admin', 'gimana tu', '0', '2026-07-11 02:04:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `nama_customer` varchar(100) DEFAULT NULL,
+  `wa` varchar(20) DEFAULT NULL,
+  `nickname` varchar(100) DEFAULT NULL,
+  `uid` varchar(100) DEFAULT NULL,
+  `jenis_joki` enum('Boss','Dungeon','Rift') DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL,
+  `total` int(11) DEFAULT NULL,
+  `catatan` text DEFAULT NULL,
+  `status` enum('Pending','Diproses','Selesai') DEFAULT 'Pending',
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `nama_customer`, `wa`, `nickname`, `uid`, `jenis_joki`, `jumlah`, `harga`, `total`, `catatan`, `status`, `tanggal`) VALUES
+(1, 3, 'adit', '112244231225', 'adit', '', 'Boss', 20, 2000, 40000, 'halo mas', 'Pending', '2026-07-11 00:57:27'),
+(2, 4, 'adit', '', 'aa', '', 'Boss', 6, 2000, 12000, 'aaa', 'Pending', '2026-07-11 01:22:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengaturan`
+--
+
+CREATE TABLE `pengaturan` (
+  `id` int(11) NOT NULL,
+  `nama_website` varchar(100) NOT NULL,
+  `harga_boss` int(11) NOT NULL DEFAULT 2000,
+  `harga_dungeon` int(11) NOT NULL DEFAULT 2000,
+  `harga_rift` int(11) NOT NULL DEFAULT 5000,
+  `wa_admin` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pengaturan`
+--
+
+INSERT INTO `pengaturan` (`id`, `nama_website`, `harga_boss`, `harga_dungeon`, `harga_rift`, `wa_admin`) VALUES
+(1, 'Catch A Monster Joki', 2000, 2000, 0, '08123456789');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `role` enum('admin','customer') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
+(1, 'admin', '12345', 'admin'),
+(2, 'farhan', '12341', 'customer'),
+(3, 'riski', '1221', 'customer'),
+(4, 'abdul', '12345', 'customer');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pengaturan`
+--
+ALTER TABLE `pengaturan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `pengaturan`
+--
+ALTER TABLE `pengaturan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
